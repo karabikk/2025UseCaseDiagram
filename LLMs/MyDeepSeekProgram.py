be61a036a5aa956d026d97c6d4921fa19640b3ba
karabikk <158528794+karabikk@users.noreply.github.com>
2025-12-22 18:40:58 -0500

    Add files via upload

---
+++ LLMs/MyDeepSeekProgram.py
@@ -0,0 +1,56 @@
+import os
+import openai
+import pandas as pd
+import re
+
+
+api_key = os.getenv("DEEKSEEK_API_KEY")
+client = openai.OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+df = pd.read_csv("law_requirements.csv")
+functional_requirements = "\n".join(df["RequirementID"] + ": " + df["Requirement"])
+print(functional_requirements)
+
+response= client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[              
+                {"role": "user", "content": f"""You are a Software System Engineer using the IEEE 29148 SRS standard. Your task is to rewrite software functional requirements into a professional system requirement. {functional_requirements}"""}
+             ]    
+)
+chain1 = response.choices[0].message.content.strip()
+print(chain1)
+
+response2 = client.chat.completions.create(
+     model="deepseek-chat",
+     messages=[
+         {"role": "user", "content":f"You are a Software Systems Engineer who is given the following task: please extract actors from the functional requirements and describe their goals. Then convert their goals into use cases. Output only the use cases. {chain1}."}
+     ]
+)
+chain2 = response2.choices[0].message.content.strip()
+print(chain2)
+
+response3 = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+         {"role": "user", "content":f" You are a Software Systems Engineer tasked with designing a UML use case diagram; given the following use cases, decide the appropriate relationships between them and then create a use case diagram using PlantUML notation. {chain2}"}
+    ]
+)
+chain3 = response3.choices[0].message.content.strip()
+print(chain3)
+
+response4 = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+                {"role": "user", "content":f" You are a Software Systems Engineer who is assigned the following: remove any actor-to-use case associations where the use case is already connected through an <<extend>> or <<include>> relationship. {chain3}."}
+    ]
+)
+chain4 = response4.choices[0].message.content.strip()
+print(chain4)
+
+response5 = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+                {"role": "user", "content":f" You are given three tasks. Your first task is to convert the actor use case association arrows into plain, non-directional lines. Your second task is to find any <<extend>> relationship and rewrite it using a dotted arrow with UP directional modifier using this format: X .up.> Y : <<extend>> Your third task is find any <<include>> relationship and rewrite it using a dotted arrow with DOWN directional modifier using this format: X .down.> Y :<<include>> {chain4}"}
+    ]
+)
+chain5 = response5.choices[0].message.content.strip()
+print(chain5)
